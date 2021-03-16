4835626f23c52f8e9a20d3ae51183fe5bbbac967
zvrkhanuma <almazalovic@gmail.com>
2021-03-16 23:31:23 +0100

    Added logicalAndOperator

---
+++ ex07/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+function logicalAndOperator(num){
+    if (num >= 40 && num <= 80){
+        return "Yes";
+    }
+    return "No";
+}
+logicalAndOperator(0);
+logicalAndOperator(24);
+logicalAndOperator(45);
+logicalAndOperator(57);
+logicalAndOperator(100);
+
+console.log(logicalAndOperator(0));
+console.log(logicalAndOperator(24));
+console.log(logicalAndOperator(45));
+console.log(logicalAndOperator(57));
+console.log(logicalAndOperator(100));
+// Only change code above this line
+module.exports = logicalAndOperator;

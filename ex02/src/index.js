86acacc4bd4a42ae7a5c4a802d5146961f990349
zvrkhanuma <almazalovic@gmail.com>
2021-03-16 21:46:59 +0100

    Added second index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myFunction(myParameter){
+    if (myParameter){
+        return "The parameter is true!";
+    }
+    return "The parameter is false!";
+}
+myFunction(true);
+myFunction(false);
+console.log(myFunction(true));
+console.log(myFunction(false));
+// Only change code above this line
+module.exports = myFunction;

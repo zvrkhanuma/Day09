710c0ad8a3cbf0bfdba274394dfecd29a4c48daa
zvrkhanuma <almazalovic@gmail.com>
2021-03-16 21:28:28 +0100

    Added true function

---
+++ ex01/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function myTrueBoolean(){
+    return true;
+}
+myTrueBoolean();
+console.log(myTrueBoolean());
+// Only change code above this line
+
+module.exports = myTrueBoolean;

08d1dc3e86c6d3eb77c29d0a5dba4df6e04e18f4
zvrkhanuma <almazalovic@gmail.com>
2021-03-17 10:08:03 +0100

    Added myDoWhile function

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function myDoWhile() {
+    var myNumbers = 0;
+    do {console.log(myNumbers);
+        myNumbers++;
+    }
+    while(myNumbers <= 9);
+    }
+    myDoWhile();
+    console.log(myDoWhile());
+    // Only change code above this line
+    module.exports = myDoWhile;

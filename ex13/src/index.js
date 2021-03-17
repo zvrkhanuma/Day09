83c1c6b6cd2f873cf9d8670ff0c2018c96445e5c
zvrkhanuma <almazalovic@gmail.com>
2021-03-17 09:13:11 +0100

    Added inverseWhile function

---
+++ ex13/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+
+function inverseWhile(){
+    var fiveNumber = 5;
+    while (fiveNumber >-1){
+        console.log(fiveNumber);
+        fiveNumber--;
+    }
+
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
